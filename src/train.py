0885f122511d2fb6636dfcd7eda9b5dbccce4fd9
ramlaoui <ramlaoui@outlook.com>
2024-03-27 17:17:55 +0000

    add metrics and measure invariance

--- src/train.py
+++ src/train.py
@@ -8,7 +8,7 @@ from torch.optim.lr_scheduler import CosineAnnealingLR
 from .datasets.base_dataset import BaseDataset, ParallelCollater
 from .modules.frame_averaging import FrameAveraging
 from .faenet import FAENet
-from .datasets.data_utils import Normalizer
+from .datasets.data_utils import Normalizer, GraphRotate, GraphReflect
 from torchvision.transforms import Compose
 
 def transformations_list(config):
@@ -74,22 +74,21 @@ class Trainer():
                 self.normalizer = None
 
         self.parallel_collater = ParallelCollater() # To create graph batches
-        transform = transformations_list(self.config)
-        train_dataset = BaseDataset(self.config['dataset']['train'], transform=transform)
+        self.transform = transformations_list(self.config)
+        train_dataset = BaseDataset(self.config['dataset']['train'], transform=self.transform)
         self.train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=self.config["optimizer"]['batch_size'], shuffle=True, num_workers=0, collate_fn=self.parallel_collater)
     
     def load_val_loaders(self):
         self.val_loaders = []
         for split in self.config['dataset']['val']:
-            transform = transformations_list(self.config)
-            val_dataset = BaseDataset(self.config['dataset']['val'][split], transform=transform)
+            val_dataset = BaseDataset(self.config['dataset']['val'][split], transform=self.transform)
             val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=self.config["optimizer"]['eval_batch_size'], shuffle=False, num_workers=0, collate_fn=self.parallel_collater)
             self.val_loaders.append(val_loader)
     
     def faenet_call(self, batch):
         equivariance = self.config.get("equivariance", "")
         outputs = []
-        if equivariance == "data_augmentation":
+        if equivariance != "frame_averaging":
             return self.model(batch)
         else:
             original_batch = deepcopy(batch)
@@ -108,24 +107,44 @@ class Trainer():
     def train(self):
         log_interval = self.config["optimizer"].get("log_interval", 100)
         epochs = self.config["optimizer"].get("epochs", 1)
+
+        mae = torch.nn.L1Loss()
+        mse = torch.nn.MSELoss()
+
+        run_time, n_batches = 0, 0
         for epoch in range(epochs):
             self.model.train()
             pbar = tqdm(self.train_loader)
             for batch_idx, (batch) in enumerate(pbar):
+                n_batches += len(batch[0].natoms)
                 batch = batch[0].to(self.device)
                 self.optimizer.zero_grad()
+                start_time = torch.cuda.Event(enable_timing=True)
                 output = self.faenet_call(batch)
+                end_time = torch.cuda.Event(enable_timing=True)
+                start_time.record()
+                end_time.record()
+                torch.cuda.synchronize()
+                current_run_time = start_time.elapsed_time(end_time)
+                run_time += current_run_time
                 target = batch.y_relaxed
                 if self.normalizer:
                     target_normed = self.normalizer.norm(target)
+                    output_unnormed = self.normalizer.denorm(output["energy"].reshape(-1))
                 else:
                     target_normed = target
+                    output_unnormed = output["energy"].reshape(-1)
                 loss = self.criterion(output["energy"].reshape(-1), target_normed.reshape(-1))
                 loss.backward()
+                mae_loss = mae(output_unnormed, target)
+                mse_loss = mse(output_unnormed, target)
                 grad_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1)
                 self.optimizer.step()
                 metrics = {
                     "train/loss": loss.item(),
+                    "train/mae": mae_loss.item(),
+                    "train/mse": mse_loss.item(),
+                    "train/batch_run_time": current_run_time,
                     "train/lr": self.optimizer.param_groups[0]['lr'],
                     "train/epoch": epoch,
                 }
@@ -134,15 +153,21 @@ class Trainer():
                 pbar.set_description(f'Epoch {epoch+1}/{epochs} - Loss: {loss.item():.6f}')
                 if self.scheduler:
                     self.scheduler.step()
+                break
             if self.device.type == 'cuda':
                 torch.cuda.empty_cache()
+            if not self.debug:
+                self.writer.log({"systems_per_second": 1/(run_time / n_batches)})
             if epoch != epochs-1:
                 self.validate(epoch, splits=[0]) # Validate on the first split (val_id)
         self.validate(epoch) # Validate on all splits
+        invariance_metrics = self.measure_model_invariance(self.model)
         
         
     def validate(self, epoch, splits=None):
         self.model.eval()
+        mae = torch.nn.L1Loss()
+        mse = torch.nn.MSELoss()
         for i, val_loader in enumerate(self.val_loaders):
             if splits and i not in splits:
                 continue
@@ -154,12 +179,72 @@ class Trainer():
                 output = self.faenet_call(batch)
                 target = batch.y_relaxed
                 if self.normalizer:
-                    target_normed = self.normalizer.norm(target)
+                    output_unnormed = self.normalizer.denorm(output["energy"].reshape(-1))
                 else:
-                    target_normed = target
-                loss = self.criterion(output["energy"].reshape(-1), target_normed.reshape(-1))
-                total_loss += loss.item()
-                pbar.set_description(f'Val {i} - Epoch {epoch+1} - Loss: {loss.item():.6f}')
+                    output_unnormed = output["energy"].reshape(-1)
+                # loss = self.criterion(output["energy"].reshape(-1), target_normed.reshape(-1))
+                mae_loss = mae(output_unnormed, target)
+                mse_loss = mse(output_unnormed, target)
+                pbar.set_description(f'Val {i} - Epoch {epoch+1} - MAE: {mae_loss.item():.6f}')
             total_loss /= len(val_loader)
             if not self.debug:
-                self.writer.log({f"{split}/loss": total_loss, f"{split}/epoch": epoch})
+                self.writer.log({
+                    f"{split}/mae": mae_loss.item(),
+                    f"{split}/mse": mse_loss.item()
+                })
+
+    def measure_model_invariance(self, model):
+        model.eval()
+        metrics = {}
+
+        rotater_2d = GraphRotate(-180, 180, [2])
+        rotater_3d = GraphRotate(-180, 180, [0, 1, 2])
+        reflector = GraphReflect()
+
+        energy_delta_rotated_2d, energy_delta_reflected, energy_delta_rotated_3d = 0, 0, 0
+        n_batches = 0
+        # Testing no val_id only
+        new_val_loader = torch.utils.data.DataLoader(self.val_loaders[0].dataset, batch_size=max(self.config["optimizer"]['eval_batch_size']//4, 1), shuffle=False, num_workers=0, collate_fn=self.parallel_collater)
+        split = list(self.config['dataset']['val'].keys())[0]
+        pbar = tqdm(new_val_loader)
+        for j, batch in enumerate(pbar): 
+            batch = batch[0]
+            rotated_graph, _, _ = rotater_2d(batch)
+            rotated_graph_3d, _, _ = rotater_3d(batch)
+            reflected_graph, _, _ = reflector(batch)
+
+            n_batches += len(batch[0].natoms)
+
+            preds_original = self.faenet_call(batch.to(self.device))
+            batch = batch.detach()
+            if self.device.type == 'cuda':
+                torch.cuda.empty_cache()
+            preds_rotated = self.faenet_call(rotated_graph.to(self.device))
+            rotated_graph = rotated_graph.detach()
+            if self.device.type == 'cuda':
+                torch.cuda.empty_cache()
+            preds_rotated_3d = self.faenet_call(rotated_graph_3d.to(self.device))
+            rotated_graph_3d = rotated_graph_3d.detach()
+            if self.device.type == 'cuda':
+                torch.cuda.empty_cache()
+            preds_reflected = self.faenet_call(reflected_graph.to(self.device))
+            reflected_graph = reflected_graph.detach()
+            if self.device.type == 'cuda':
+                torch.cuda.empty_cache()
+
+            energy_delta_rotated_2d += torch.abs(preds_original["energy"] - preds_rotated["energy"]).sum().item()
+            energy_delta_reflected += torch.abs(preds_original["energy"] - preds_reflected["energy"]).sum().item()
+            energy_delta_rotated_3d += torch.abs(preds_original["energy"] - preds_rotated_3d["energy"]).sum().item()
+            pbar.set_description(f'Measuring invariance - Split {split} - Batch {j} - Energy Delta Rotated 2D: {energy_delta_rotated_2d/n_batches:.6f} - Energy Delta Reflected: {energy_delta_reflected/n_batches:.6f} - Energy Delta Rotated 3D: {energy_delta_rotated_3d/n_batches:.6f}')
+
+        metrics[f"{split}/energy_delta_rotated_2d"] = energy_delta_rotated_2d / n_batches,
+        metrics[f"{split}/energy_delta_reflected"] =  energy_delta_reflected / n_batches,
+        metrics[f"{split}/energy_delta_rotated_3d"] =  energy_delta_rotated_3d / n_batches,
+        if not self.debug:
+            self.writer.log(metrics)
+        pbar.close()
+        print("\nInvariance results:")
+        for key, value in metrics.items():
+            print(f"{key}: {value}")
+
+        return metrics
