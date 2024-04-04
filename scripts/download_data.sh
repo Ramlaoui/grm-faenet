3625cc3a3c7773c6cabfa9b2efb91191771f89e0
Ali Ramlaoui <ramlaoui@outlook.com>
2024-04-04 06:08:43 +0000

    update download of gdown for datasets dl

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -3,7 +3,7 @@ if [ "$1" = "is2re" ]; then
     cd data
     mkdir -p is2re/train/
     cd is2re/train
-    pip install gdown
+    pip install --upgrade --no-cache-dir gdown
     if [ ! -f is2re_10k.lmdb ]; then
         gdown https://drive.google.com/uc?id=19b7kOXBiHkhr_gzo-0iITIDN012puN-1 -O is2re_10k.lmdb
     fi
